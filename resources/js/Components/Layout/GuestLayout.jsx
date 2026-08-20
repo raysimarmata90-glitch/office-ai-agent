64c6b719eb8d0ac33c6188bd7b81e4fc1e0c825a
raysimarmata23@gmail.com <261098534+raysimarmata90-glitch@users.noreply.github.com>
2026-08-20 15:21:12 +0700

    drop all 2

--- resources/js/Components/Layout/GuestLayout.jsx
+++
@@ -1,7 +0,0 @@
-export default function GuestLayout({ children }) {
-    return (
-        <div className="min-h-screen bg-gradient-to-br from-indigo-50 via-white to-purple-50">
-            {children}
-        </div>
-    );
-}
